698c8eb1d67b961c11cb8714edebe2461124eb68
Burak Güner <burak.guner@callstack.com>
2023-06-06 23:08:17 +0300

    feat: list media

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use clap::Parser;
-use streamy::config::{self, Args};
+use streamy::{
+    config::{self, Args},
+    media::MediaList,
+};
+
+// This struct represents state
+struct AppState {
+    config: Args,
+}
+
+type AppData = web::Data<AppState>;
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[get("/list-media")]
+async fn list_media(state: AppData) -> impl Responder {
+    let media_list = MediaList::new(&state.config.media_path);
+    HttpResponse::Ok().json(media_list.files)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let config: Args = config::Args::parse();
-    HttpServer::new(|| App::new().service(hello))
-        .bind((config.address, config.port))?
-        .run()
-        .await
+    let config = config::Args::parse();
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                // TODO: refactor
+                config: config::Args::parse(),
+            }))
+            .service(hello)
+            .service(list_media)
+    })
+    .bind((config.address, config.port))?
+    .run()
+    .await
 }
---
+++ src/media.rs
@@ -0,0 +1,50 @@
+use std::path::PathBuf;
+use walkdir::WalkDir;
+
+/// This module actively scans for the media path and returns a list of files
+/// that can be served.
+///
+/// It
+///
+
+#[derive(Debug)]
+pub struct MediaList {
+    pub files: Vec<String>,
+}
+
+impl MediaList {
+    pub fn new(media_path: &PathBuf) -> Self {
+        MediaList {
+            files: scan_media(media_path),
+        }
+    }
+
+    pub fn on_update(&mut self) {
+        // TODO
+    }
+}
+
+/// This scans the media path and returns a list of files that can be
+/// served.
+/// TODO: Refactor
+fn scan_media(media_path: &PathBuf) -> Vec<String> {
+    WalkDir::new(media_path)
+        .into_iter()
+        .filter_map(|file| {
+            let file = file.ok()?;
+            if file.file_type().is_dir() {
+                return None;
+            }
+            if let Some(ext) = file.path().extension() {
+                if ext != "mp4" && ext != "mkv" && ext != "avi" {
+                    return None;
+                }
+            }
+            let path = file.path();
+            let path = path.to_str()?;
+            let mut path = path.to_string();
+            path.drain(..2);
+            Some(path)
+        })
+        .collect()
+}
