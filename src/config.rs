42044802925bbb12e0e05d8cd085d89a851eae88
Burak Güner <burak.guner@callstack.com>
2023-06-06 22:23:39 +0300

    feat: actix API with cli arguments

---
+++ src/config.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// Server address
+    #[clap(short, long, default_value = "127.0.0.1")]
+    pub address: String,
+
+    /// Server port
+    #[clap(short, long, default_value = "8080")]
+    pub port: u16,
+
+    /// Media path
+    #[clap(short, long, default_value = "./media")]
+    pub media_path: PathBuf,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use clap::Parser;
+use streamy::config::{self, Args};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let config: Args = config::Args::parse();
+    HttpServer::new(|| App::new().service(hello))
+        .bind((config.address, config.port))?
+        .run()
+        .await
 }
