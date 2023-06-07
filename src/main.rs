43381f6962be580a05feea88cffb08b6a72c23bc
Burak Güner <burak.guner@callstack.com>
2023-06-08 00:46:30 +0300

    feat: horrible filesystem watcher

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use std::sync::{Arc, Mutex};
+
 use actix_files::Files;
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use clap::Parser;
+use notify::Watcher;
 use streamy::{
     config::{self, Args},
     media::scan_media,
 };
 
 struct AppState {
-    config: Args,
+    media_files: Arc<Mutex<Vec<String>>>,
 }
-
 type AppData = web::Data<AppState>;
 
 #[get("/")]
@@ -19,21 +21,44 @@ async fn hello() -> impl Responder {
 
 #[get("/list-media")]
 async fn list_media(data: AppData) -> impl Responder {
-    let media_files = scan_media(&data.config.media_path);
-    HttpResponse::Ok().json(media_files)
+    if let Ok(media_files) = data.media_files.lock() {
+        return HttpResponse::Ok().json(media_files.clone());
+    }
+    return HttpResponse::InternalServerError().finish();
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = config::Args::parse();
-    let Args { address, port, .. } = config.clone();
+    let Args {
+        address,
+        port,
+        media_path,
+    } = config.clone();
+
+    let Args {
+        media_path: media_path_watcher,
+        ..
+    } = config.clone();
+
+    let media_files = Arc::new(Mutex::new(scan_media(&media_path)));
+    let media_files_watcher = media_files.clone();
+
+    let mut watcher = notify::recommended_watcher(move |_| {
+        if let Ok(mut media_files) = media_files_watcher.lock() {
+            *media_files = scan_media(&media_path_watcher);
+        }
+    })
+    .unwrap();
+    watcher
+        .watch(&media_path, notify::RecursiveMode::Recursive)
+        .unwrap();
 
     let server = HttpServer::new(move || {
-        let state = AppState {
-            config: config.clone(),
-        };
-
         let files_service = Files::new("/media", &config.media_path).show_files_listing();
+        let state = web::Data::new(AppState {
+            media_files: media_files.clone(),
+        });
 
         App::new()
             .app_data(state)
